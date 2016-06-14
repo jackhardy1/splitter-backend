9c4523c57e2c7987cb1b001875f1a173e1cf133b
Noah Pollock <pollocknoah@gmail.com>
2016-06-14 11:05:33 +0100

    creates item with name and price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613161937) do
+ActiveRecord::Schema.define(version: 20160614100003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160613161937) do
     t.string   "event"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.float    "price"
   end
 
 end
--- spec/unit/bill_spec.rb
+++ spec/unit/bill_spec.rb
@@ -2,8 +2,8 @@ describe Bill do
 
   it 'creates a new bill' do
     event_name = "stringfellows"
-    Bill.create(event: event_name)
+    Bill.create event: event_name
     my_bill = Bill.first
-    expect(my_bill.event).to eq(event_name)
+    expect(my_bill.event).to eq event_name
   end
 end
