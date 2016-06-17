e0f17dde510fe9aaf61956fd83cb3df4f35654b5
Noah Pollock <pollocknoah@gmail.com>
2016-06-17 15:53:28 +0100

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
-  }
+  }g
   # Code is not reloaded between requests.
   config.cache_classes = true
 
