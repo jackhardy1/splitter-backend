9f70dea857c40c9fd9be7873ef260c25131557d0
Noah Pollock <pollocknoah@gmail.com>
2016-06-20 14:30:49 +0100

    creates a signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
 end
