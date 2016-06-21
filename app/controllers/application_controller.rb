6b5c848519296bff8d80331c4c8ac5b586559627
Jack Hardy <jackhardy1@gmail.com>
2016-06-21 10:17:13 +0100

    updated files tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,37 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_filter :add_allow_credentials_headers
+  skip_before_filter :verify_authenticity_token
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = '1728000'
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
+  def add_allow_credentials_headers
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5
+    #
+    # Because we want our front-end to send cookies to allow the API to be authenticated
+    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so
+    # the browser will not reject the response
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,10 @@ Bundler.require(*Rails.groups)
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
        origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :delete]
+       resource '*',
+             :headers => :any,
+             :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+             :methods => [:get, :post, :delete, :put, :options]
     end
   end
 
